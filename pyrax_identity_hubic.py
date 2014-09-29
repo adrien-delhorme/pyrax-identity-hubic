3c3a303ae8ace1b7c34b2adfa40183c007cdf384
Nicolas Mugnier <nmugnier@maatg.fr>
2014-09-29 13:08:15 +0200

    handle refresh token

--- pyrax_identity_hubic.py
+++ pyrax_identity_hubic.py
@@ -8,6 +8,7 @@ import pyrax.exceptions as exc
 import requests
 import re
 import urlparse
+import ConfigParser
 from pyrax.base_identity import BaseIdentity, Service
 from requests.compat import quote, quote_plus
 
@@ -59,56 +60,8 @@ class HubicIdentity(BaseIdentity):
         qs = dict(urlparse.parse_qsl(query))
         return {'error': qs['error'], 'error_description': qs['error_description']}
 
-    def authenticate(self):
-#        import httplib
-#        httplib.HTTPConnection.debuglevel = 1
-        r = requests.get(
-            OAUTH_ENDPOINT+'auth/?client_id={0}&redirect_uri={1}'
-            '&scope=credentials.r,account.r&response_type=code&state={2}'.format(
-                quote(self._client_id),
-                quote_plus(self._redirect_uri),
-                pyrax.utils.random_ascii() # csrf ? wut ?..
-            ),
-            allow_redirects=False
-        )
-        if r.status_code != 200:
-            raise exc.AuthenticationFailed("Incorrect/unauthorized "
-                    "client_id (%s)"%str(self._parse_error(r)))
-
-        try:
-            from lxml import html as lxml_html
-        except ImportError:
-            lxml_html = None
-
-        if lxml_html:
-            oauth = lxml_html.document_fromstring(r.content).xpath('//input[@name="oauth"]')
-            oauth = oauth[0].value if oauth else None
-        else:
-            oauth = re.search(r'<input\s+[^>]*name=[\'"]?oauth[\'"]?\s+[^>]*value=[\'"]?(\d+)[\'"]?>', r.content)
-            oauth = oauth.group(1) if oauth else None
-
-        if not oauth:
-            raise exc.AuthenticationFailed("Unable to get oauth_id from authorization page")
-
-        r = requests.post(
-            OAUTH_ENDPOINT+'auth/',
-            data = {
-                'action': 'accepted',
-                'oauth': oauth,
-                'login': self._email,
-                'user_pwd': self._password,
-                'account': 'r',
-                'credentials': 'r',
-
-            },
-            allow_redirects=False
-        )
-
-        try:
-            query = urlparse.urlsplit(r.headers['location']).query
-            code = dict(urlparse.parse_qsl(query))['code']
-        except:
-            raise exc.AuthenticationFailed("Unable to authorize client_id, invalid login/password ?")
+    def _get_access_token(self, code):
+        print "_get_access_token()"
 
         r = requests.post(
             OAUTH_ENDPOINT+'token/',
@@ -130,6 +83,147 @@ class HubicIdentity(BaseIdentity):
                                            "wrong client_id or client_secret ? (%s)"%str(err))
 
         oauth_token = r.json()
+
+        config = ConfigParser.ConfigParser()
+        config.read('.pyrax-hubic.cfg')
+
+        if oauth_token['access_token'] is not None:
+             print "access_token is:"
+             print oauth_token['access_token']
+             config.set("hubic", "access_token", oauth_token['access_token'])
+             with open('.pyrax-hubic.cfg', 'wb') as configfile:
+                config.write(configfile)
+        else:
+             raise exc.AuthenticationFailed("Unable to get oauth access token, wrong client_id or client_secret ? (%s)"%str(err))
+
+        if oauth_token['refresh_token'] is not None:
+             print "refresh_token is:"
+             print oauth_token['refresh_token']
+             config.set("hubic", "refresh_token", oauth_token['refresh_token'])
+             with open('.pyrax-hubic.cfg', 'wb') as configfile:
+                config.write(configfile)
+        else:
+             raise exc.AuthenticationFailed("Unable to get the refresh token.")
+
+        return oauth_token
+
+    def _refresh_access_token(self):
+        print "refresh_access_token()"
+
+        config = ConfigParser.ConfigParser()
+        config.read('.pyrax-hubic.cfg')
+        refresh_token = config.get("hubic", "refresh_token")
+
+        if refresh_token is None:
+            raise exc.AuthenticationFailed("refresh_token is null. Not acquiered before ?")
+
+        r = requests.post(
+            OAUTH_ENDPOINT+'token/',
+            data={
+                'refresh_token': refresh_token,
+                'grant_type': 'refresh_token',
+            },
+            auth=(self._client_id, self._client_secret)
+        )
+        if r.status_code != 200:
+            try:
+                err = r.json()
+                err['code'] = r.status_code
+            except:
+                err = {}
+
+            raise exc.AuthenticationFailed("Unable to get oauth access token, "
+                                           "wrong client_id or client_secret ? (%s)"%str(err))
+
+        oauth_token = r.json()
+
+        if oauth_token['access_token'] is not None:
+            return oauth_token
+        else:
+            raise exc.AuthenticationFailed("Unable to get oauth access token from json")
+
+
+    def authenticate(self):
+#        import httplib
+#        httplib.HTTPConnection.debuglevel = 1
+
+        config = ConfigParser.ConfigParser()
+        config.read('.pyrax-hubic.cfg')
+        refresh_token = config.get("hubic", "refresh_token")
+
+        if refresh_token is not None:
+            oauth_token = self._refresh_access_token()
+        else:
+            r = requests.get(
+                OAUTH_ENDPOINT+'auth/?client_id={0}&redirect_uri={1}'
+                '&scope=credentials.r,account.r&response_type=code&state={2}'.format(
+                    quote(self._client_id),
+                    quote_plus(self._redirect_uri),
+                    pyrax.utils.random_ascii() # csrf ? wut ?..
+                ),
+                allow_redirects=False
+            )
+            if r.status_code != 200:
+                raise exc.AuthenticationFailed("Incorrect/unauthorized "
+                        "client_id (%s)"%str(self._parse_error(r)))
+
+            try:
+                from lxml import html as lxml_html
+            except ImportError:
+                lxml_html = None
+
+            if lxml_html:
+                oauth = lxml_html.document_fromstring(r.content).xpath('//input[@name="oauth"]')
+                oauth = oauth[0].value if oauth else None
+            else:
+                oauth = re.search(r'<input\s+[^>]*name=[\'"]?oauth[\'"]?\s+[^>]*value=[\'"]?(\d+)[\'"]?>', r.content)
+                oauth = oauth.group(1) if oauth else None
+
+            if not oauth:
+                raise exc.AuthenticationFailed("Unable to get oauth_id from authorization page")
+
+            r = requests.post(
+                OAUTH_ENDPOINT+'auth/',
+                data = {
+                    'action': 'accepted',
+                    'oauth': oauth,
+                    'login': self._email,
+                    'user_pwd': self._password,
+                    'account': 'r',
+                    'credentials': 'r',
+
+                },
+                allow_redirects=False
+            )
+
+            try:
+                query = urlparse.urlsplit(r.headers['location']).query
+                code = dict(urlparse.parse_qsl(query))['code']
+            except:
+                raise exc.AuthenticationFailed("Unable to authorize client_id, invalid login/password ?")
+
+           # r = requests.post(
+           #     OAUTH_ENDPOINT+'token/',
+           #     data={
+           #         'code': code,
+           #         'redirect_uri': self._redirect_uri,
+           #         'grant_type': 'authorization_code',
+           #     },
+           #     auth=(self._client_id, self._client_secret)
+           # )
+           # if r.status_code != 200:
+           #     try:
+           #         err = r.json()
+           #         err['code'] = r.status_code
+           #     except:
+           #         err = {}
+
+           #     raise exc.AuthenticationFailed("Unable to get oauth access token, "
+           #                                    "wrong client_id or client_secret ? (%s)"%str(err))
+
+           # oauth_token = r.json()
+            oauth_token = self._get_access_token(code)
+
         if oauth_token['token_type'].lower() != 'bearer':
             raise exc.AuthenticationFailed("Unsupported access token type")
 
